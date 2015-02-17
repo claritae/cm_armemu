6ca3476c0410dd611e0bf2063c496893479cbb6c
Roger Ye <shugaoye@yahoo.com>
2015-02-17 16:27:35 +0800

    Integrated kernel and U-Boot.

--- device_armemu.mk
+++ device_armemu.mk
@@ -14,8 +14,22 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+# Build U-Boot
+PRODUCT_OUT ?= out/target/product/armemu
+TARGET_U_BOOT_SOURCE := u-boot
+TARGET_U_BOOT_CONFIG := goldfish_config
+
+include $(TARGET_U_BOOT_SOURCE)/AndroidU-Boot.mk
+$(PRODUCT_OUT)/u-boot.bin: $(TARGET_PREBUILT_U-BOOT)
+	cp $(TARGET_PREBUILT_U-BOOT) $(PRODUCT_OUT)/u-boot.bin
+
+LOCAL_U_BOOT := $(TARGET_PREBUILT_U-BOOT)
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+     $(LOCAL_U_BOOT):u-boot.bin \
+     $(LOCAL_KERNEL):kernel \
+     device/generic/armemu/init.recovery.armemu.rc:root/init.recovery.armemu.rc \
+     device/generic/armemu/init.recovery.armemu.sh:root/init.recovery.armemu.sh
 
 $(call inherit-product, build/target/product/full.mk)
 
